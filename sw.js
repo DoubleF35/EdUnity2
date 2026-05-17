54289c8ca2d695ae0c38c81159ef04fa9c0bca2f
DoubleF35 <fedefas.210@gmail.com>
2026-05-17 21:34:55 +0200

    Update service worker version to v6.6

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //  versione qui sotto (es. v3, v4...). È questo cambiamento che dice
 //  al browser "c'è una versione nuova, scaricala".
 // ═══════════════════════════════════════════════════════════════════
-const VERSION = 'edunity-v6.5';
+const VERSION = 'edunity-v6.6';
 const CACHE = VERSION;
 
 // File di base da avere disponibili offline
