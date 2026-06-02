6fd037e462bc83613cac75f5123a6ce9ee2ca044
DoubleF35 <fedefas.210@gmail.com>
2026-06-02 12:42:50 +0200

    Update service worker version to v36
    Increment version number for service worker.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //  versione qui sotto (es. v3, v4...). È questo cambiamento che dice
 //  al browser "c'è una versione nuova, scaricala".
 // ═══════════════════════════════════════════════════════════════════
-const VERSION = 'edunity-v35';
+const VERSION = 'edunity-v36';
 const CACHE = VERSION;
 
 // File di base da avere disponibili offline
